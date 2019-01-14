f1dc88c26128d4d07436a34ed29cae5deaa63535
Alan Rainot <alan.rainot@kuleuven.be>
2019-01-14 15:17:35 +0100

    First commit

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from __future__ import (absolute_import)
+
+from . import preproc
+from . import conf
+from . import fits
+from . import llsg
+from . import madi
+from . import negfc
+from . import nmf
+from . import pca
+from . import phot
+from . import stats
+from . import var
+
+__version__ = "0.1.0"
+
+print("                                    ")
+print("   _____ _    _ _____ _____   _____ ")
+print("  / ____| |  | |_   _|  __ \ / ____|")
+print(" | (___ | |__| | | | | |__) | (___  ")
+print("  \___ \|  __  | | | |  ___/ \___ \ ")
+print("  ____) | |  | |_| |_| |     ____) |")
+print(" |_____/|_|  |_|_____|_|    |_____/ ")
+print("                                    ")
+print(" SPHERE High-contrast Imaging Pipeline for massive Stars "+__version__)
+print("                                                   ")
+print("Please cite Rainot et al. ")
+print("whenever you publish data with SHIPS.")
